cd993df4a3f5f4e28e212f361d02fcfd19d25a65
amaboh <amabocanon@gmail.com>
2022-04-27 07:15:13 +0100

    updates on clean code structure using MVC

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -5,26 +5,26 @@ const Blog = require('../models/blog')
 const blog_index = (req,res)=>{
     Blog.find().sort({createdAt: -1})
     .then((result)=>{
-        res.render('index', {title: 'All Blogs', blogs: result});
+        res.render('blogs/index', {title: 'All Blogs', blogs: result});
     })
     .catch((err)=>{
         console.log(err)
     })
 }
 
-const blog_details = (req,res)=>{
+const blog_details = (req, res) => {
     const id = req.params.id;
-      Blog.findById(id)
-        .then(result => {
-            res.render('details', {blog: result, title: 'Blog Details '})
-        })
-        .catch(err => {
-            console.log(err)
-        });
-}
+    Blog.findById(id)
+      .then(result => {
+        res.render('blogs/details', { blog: result, title: 'Blog Details' });
+      })
+      .catch(err => {
+        res.status(404).render('404', {title: 'Blog not found'})
+      });
+  }
 
 const blog_create_get = (req, res) => {
-    res.render('create', {title: 'Create a new blog'})
+    res.render('blogs/create', {title: 'Create a new blog'})
 }
 
 
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const blogController = require('../controllers/blogController')
+const express = require('express');
+const blogController = require('../controllers/blogController');
 
 
 const router = express.Router()
@@ -8,7 +8,7 @@ const router = express.Router()
 router.get('/', blogController.blog_index);
 router.post('/', blogController.blog_create_post);
 router.get('/create', blogController.blog_create_get)
-router.get('/:id', blogController.blog_details)
+router.get('/:id', blogController.blog_details);
 router.delete('/:id', blogController.blog_delete); 
 
 module.exports = router;
